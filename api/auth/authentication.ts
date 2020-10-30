fec16c16930bfddb6c82dd4cc26e14476f0704f0
saisumith770 <saisumith812@gmail.com>
2020-10-30 11:38:07 +0400

    setup auth router

---
+++ api/auth/authentication.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from 'express'
+import { login, registration } from '../../index'
+
+import { Router as OauthRouter } from './oauth'
+
+//initiating the AuthRouter
+export const Router = express.Router()
+
+//the router middlewares within auth
+Router.use('/oauth2', OauthRouter)
+
+//login route that will send access_token and refresh token
+Router.post('/login', (req: Request, res: Response) => {
+    login({
+        username: req.body.username,
+        password: req.body.password,
+        prisma: req.prisma
+    }, res)
+})
+
+//register route that will add user creds to the database.
+//note validate the creds in the front-end repo
+Router.post('/register', (req: Request, res: Response) => {
+    registration({
+        username: req.body.username,
+        password: req.body.password,
+        prisma: req.prisma
+    }, res)
+})
